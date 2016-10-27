e54d4c472ed7aec68e8177aca9b4fbe1bf1d1a0b
caojiayan <136665305@qq.com>
2016-10-27 18:26:51 +0800

    完善了“时间日历”部分
    第二十八次提交“ruanxinPC-serve”

--- js/serveIndex.js
+++ js/serveIndex.js
@@ -83,6 +83,25 @@ $(document).ready(function(){
 });
 
 //服务别称新增
+function Check(servebtn,serveName,serveList){
+	$(servebtn).click(function(){
+		var error = $('.Error');
+		var text2="请输入服务别称";
+		var text3="请输入商品别称";
+		var text=$("."+serveName).val();
+	    var text1="<li>"+text+"<a href='javascript:void();'><img src='img/delect1.png' alt='删除'></a></li>";
+	    //判断值是否为空或为输入框的提示内容
+	    if(text == ""||text == text2||text == text3){
+	    	//是，则显示提示内容
+	    	error.html("<font color='red'>提示：请输入别称!</font>");
+	    	//否，将文本追加在列表
+	    }else($(serveList).append(text1));
+	});
+	//点击删除
+    $(document).on("click", ".c2navList>li>a", function(e) {
+        $(this).parent().remove();
+    });
+}
 
 
 //交易类型
@@ -166,6 +185,31 @@ document.getElementById("cx16").style.display="none";
 document.getElementById("cx17").style.display="none";
 document.getElementById("cx18").style.display="block";
 }
+
+//服务规格新增
+function specCheck(specbtn,specName,specList,Error,specAll){
+	$(specbtn).click(function(){
+		var error = $("."+Error);
+		var text=$("."+specName).val();
+	    var text1="<li>"+text+"<a href='javascript:void();'><img src='img/delete2.png' alt='删除'></a></li>";
+	    var text2="请输入规格名称";
+		var text3="请输入内容";
+		var text4="<div id='spec2' class='clearfix'><span class='specn'><span>"+text+"</span><input type='text' class='specName2' value='请输入内容' onfocus='javascript:if(this.value=='请输入内容')this.value='';'/><input type='button' id='specbtn2' value='新增'><span class='Error2' style='font-size: 14px;'></span></span><ul class='c4Ul' id='specList2'></ul>";
+//		alert(text4);
+	    //判断值是否为空或为输入框的提示内容
+	    if(text == ""||text == text2||text == text3){
+	    	//是，则显示提示内容
+	    	error.html("<font color='red'>提示：请输入别称!</font>");
+	    	//否，将文本追加在列表
+	    }else($(specList).append(text1)&&$(specAll).append(text4));
+	});
+	//点击删除
+    $(document).on("click",specList+">.c4Ul>li>a", function(e) {
+        $(this).parent().remove();
+    });
+}
+
+
 
 //服务规格SKU列表
 function allCheck(allcheck,listBox,ulName,delectbtn){
@@ -424,12 +468,7 @@ function fileSelect() {
 
 
 //点击"删除"，删除当列
-//别称
-$(function(){
-	$(".c2navList>li>a").click(function(){
-		$(this).parent().remove();
-	});
-});
+
 //规格
 $(function(){
 	$(".c4Ul>li>a").click(function(){
